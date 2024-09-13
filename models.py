198aeeb3d1b564debe2891e2b027cf7a494e4ba2
PsaAI <james@pseudoatelier.com>
2024-09-13 16:08:24 +0930

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,16 @@
+# models.py
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime, Integer
+
+Base = declarative_base()
+
+class Video(Base):
+    __tablename__ = 'videos'
+    id = Column(Integer, primary_key=True)
+    video_id = Column(String, unique=True)
+    title = Column(String)
+    link = Column(String)
+    published_at = Column(DateTime)
+    channel_id = Column(String)
 
